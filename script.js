a7140b4ddeb2be282ae7e2f6470d9e6fe1122fc4
Retha <163494945+Rethaaa@users.noreply.github.com>
2025-04-03 16:28:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// preloader
+var loader = document.querySelector("#preloader");
+window.addEventListener("load", function() {
+    loader.style.display = "none";
+});
+
+// navbar
+const navView = document.querySelector("#nav-view");
+let navList = document.querySelector(".nav-list");
+navView.onclick = function() {
+    navList.classList.toggle("slide");
+}
+if (window.innerWidth <= 1194) {
+    const dropdowns = document.querySelectorAll(".dropdown");
+    dropdowns.forEach(dropdown => {
+        dropdown.addEventListener("click", event => {
+            dropdown.classList.toggle("open");
+        });
+    });
+}
